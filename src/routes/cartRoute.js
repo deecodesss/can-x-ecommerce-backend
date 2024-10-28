6e54ec3b990641ad43bb9fc7513f8306c7798675
Deeksha Agarwal <agarwaldeeksha03@gmail.com>
2024-10-28 12:15:26 +0530

    feat: last used address

--- src/routes/cartRoute.js
+++ src/routes/cartRoute.js
@@ -7,6 +7,7 @@ const {
   applyCoupon,
   getCoupon,
   removeCouponFromCart,
+  getLastUsedAddress,
 } = require("../controllers/cartControllers");
 const router = express.Router();
 
@@ -17,5 +18,6 @@ router.put("/decreaseQuantity", decreaseQuantityOfProduct);
 router.post("/applyCoupon", applyCoupon);
 router.get("/getCoupon/:userId", getCoupon);
 router.delete("/removeCoupon/:userId", removeCouponFromCart);
+router.get("/lastUsedAddress/:userId", getLastUsedAddress);
 
 module.exports = router;
